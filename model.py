60d481e7f3d4a99f1e692c42e15631e2df216f3b
Frankcisco-Xavier <frankcisco-xavier@live.com.mx>
2016-12-11 21:09:52 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import web, datetime
 
-db = web.database(dbn='postgres', host='ec2-54-247-123-125.eu-west-1.compute.amazonaws.com', port='5432', db='d5itkm81gralmc',pw='_e6Nt9LbnJaOWf65eM5ARIMC2n', user='gwznfrjkovdlzj')
+db = web.database(dbn='postgres', host='ec2-54-163-254-48.compute-1.amazonaws.com', port='5432', db='d5udq2a6eihpmb',pw='61de49a2f52ead35a8bd8ca6d865eba6f202c101f8325b756e3dbab54e7f28e7', user='nadzfbbvljbjrd')
 
 def get_posts():
     return db.select('contactos', order='id_contacto DESC')
